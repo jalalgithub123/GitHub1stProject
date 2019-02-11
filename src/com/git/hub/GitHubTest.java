c69555a185f0a0c2a9e1f69c1ed18d92a5c37419
jalal8068 <jalalahamad9@gmail.com>
2019-02-11 21:55:06 +0530

    made change in 1st project

--- src/com/git/hub/GitHubTest.java
+++ src/com/git/hub/GitHubTest.java
@@ -4,7 +4,7 @@ public class GitHubTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("1ST GIT HUB TEST");
+System.out.println("1ST GIT HUB TEST CORRECTION");
 	}
 
 }
